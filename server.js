022006e86cf5bb9845f89da211cb903b69b19d2b
Chinkuza <j.rosarioc2h@gmail.com>
2019-11-27 11:54:17 -0500

    heroku ready

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 var express = require("express");
 var mongoose = require("mongoose");
 
@@ -11,9 +13,12 @@ app.use(express.json());
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/imageperformance", {
-  useNewUrlParser: true
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/imageperformance",
+  {
+    useNewUrlParser: true
+  }
+);
 
 app.listen(PORT, function() {
   console.log(`Now listening on port: ${PORT}`);
